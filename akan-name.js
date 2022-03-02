03e7901facf88be4c3f5f3dead72545659ddff31
Ruth Moraa <moraar37@gmail.com>
2022-03-02 05:45:27 +0300

    adding dayValidator to my javascript

--- akan-name.js
+++ akan-name.js
@@ -12,4 +12,28 @@ let daysOfWeek = ["sunday", "monday", "tuesday", "wednesday", "thursday", "frida
 
 let maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 
-let femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+let femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+function dayValidator () {
+    if (month === 2 && Number(yearOfBirth)%4 === 0){
+        alert ("please write your name!");
+        if(dayOfBirth > 28 || dayOfBirth < 1){
+            return false;
+        }
+    else if (month === 2 && dayOfBirth > 29) {
+        return false;
+    }
+    else if (month === 2 && dayOfBirth < 1) {
+        return false;
+    }
+    else {
+        return true;
+    }
+
+    } else if (dayOfBirth < 1 || dayOfBirth > 31){
+        return false;
+    } else {
+        return true;
+    }
+
+}
