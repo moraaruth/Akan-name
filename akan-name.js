7eaaf004413fbc688fec78c5505583e5fa836707
Ruth Moraa <moraar37@gmail.com>
2022-03-04 20:59:38 +0300

    editing my form for easy input

--- akan-name.js
+++ akan-name.js
@@ -1,11 +1,5 @@
 function setAction(form) {
-    form.action = "akan-name.html";
+    form.action = "akanName.html";
     alert(form.action);
-    return false;
-    }
-    
-    function akanName() {
-    document.user_form.action = "akan-name.html";
-    alert(document.user_form.action);
     return false;
     }
