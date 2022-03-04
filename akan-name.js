b487ef233fc8705bf54fbcf8fa459df55c39c5aa
Ruth Moraa <moraar37@gmail.com>
2022-03-04 05:39:04 +0300

    adding javascript to my form

--- akan-name.js
+++ akan-name.js
@@ -1,48 +1,11 @@
-function getAkanName(){
-    let dateOfBirth = Number(document.getElementById("day").value);
-    let monthOfBirth = Number(document.getElementById("month").value);
-    let yearOfBirth = document.getElementById("year").value;
-    let genders = document.getElementById("gender");
-
-}
-
-//arrays
-
-let daysOfWeek = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
-
-let maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-
-let femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-
-function dayValidator () {
-    if (month === 2 && Number(yearOfBirth)%4 === 0){
-        alert ("please write your name!");
-        if(dayOfBirth > 28 || dayOfBirth < 1){
-            return false;
-        }
-    else if (month === 2 && dayOfBirth > 29) {
-        return false;
+function setAction(form) {
+    form.action = "akan-name.html";
+    alert(form.action);
+    return false;
     }
-    else if (month === 2 && dayOfBirth < 1) {
-        return false;
+    
+    function akanName() {
+    document.user_form.action = "akan-name.html";
+    alert(document.user_form.action);
+    return false;
     }
-    else {
-        return true;
-    }
-
-    } else if (dayOfBirth < 1 || dayOfBirth > 31){
-        return false;
-    } else {
-        return true;
-    }
-
-}
-
-function monthValidator () {
-    if (month < 1 || month > 12){
-        return false;
-    } else {
-        return true;
-    }
-
-}
