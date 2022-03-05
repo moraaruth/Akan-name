a3e8ce01223fa8b2baaefdf0d26c8d44a60cbe2a
Ruth Moraa <moraar37@gmail.com>
2022-03-05 06:00:49 +0300

    adding validators i.e monthValidator and dayValidator to my project

--- akan-name.js
+++ akan-name.js
@@ -21,77 +21,34 @@ function getAkanName () {
     let myGender = getGender();
     console.log(myGender);
   
-  
     function monthValidator () {
-      if (monthOfBirth < 1 || monthOfBirth > 12) {
-        return false;
-      } else {
-        return true;
-      }
-    }
-  
-    function dayValidator () {
-      if (monthOfBirth === 2 && Number(yearOfBirth)%4 === 0) {
-        if (dayOfBirth > 28 || dayOfBirth < 1) {
-          return false;
-        } else if (monthOfBirth === 2 && dayOfBirth > 29) {
-          return false;
-        } else if (monthOfBirth === 2 && dayOfBirth < 1) {
+        if (monthOfBirth < 1 || monthOfBirth > 12) {
           return false;
         } else {
           return true;
         }
-      } else if (dayOfBirth < 1 || dayOfBirth > 31){
-        return false;
-      } else {
-        return true;
       }
-    }
-  
-   
-    let monthValid = monthValidator();
-    let dayValid = dayValidator();
-  
-    .
-    let dayOfWeekNumber = Math.floor((((Number(yearOfBirth.slice(0,2))/4)-2*Number(yearOfBirth.slice(0,2))-1)+
-            ((5*Number(yearOfBirth.slice(2,4))/4))+((26*(monthOfBirth+1)/10))+dayOfBirth)%7);
-  
-    // Akan names for males & females and days of the week
-    let daysOfWeek = [
-      "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
-    ];
-  
-    let maleAkanNames = [
-      "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"
-    ];
-  
-    let femaleAkanNames = [
-      "Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"
-    ];
-  
-    //selecting items on arrays
-    let index;
     
-    if (dayOfWeekNumber == 0){
-      index = 6;
-    } else {
-      index = dayOfWeekNumber - 1;
-    }
-  
-    alert(index);
-  
-  
-    if (myGenderValue == "male" && monthValid && dayValid) {
-        document.getElementById('result').innerHTML = ("You were born on a " + daysOfWeek[index] + " , your Akan name is " + maleAkanNames[index]);
-      
-        return false;
-      } else if (myGenderValue == "female" && monthValid && dayValid) {
-        document.getElementById('result').innerHTML =( "You were born on a " + daysOfWeek[index] + " , your Akan name is " + femaleAkanNames[index]);
-  
-        return false;
-      } else {
-        alert("You entered an invalid day or month, please try again");
+      function dayValidator () {
+        if (monthOfBirth === 2 && Number(yearOfBirth)%4 === 0) {
+          if (dayOfBirth > 28 || dayOfBirth < 1) {
+            return false;
+          } else if (monthOfBirth === 2 && dayOfBirth > 29) {
+            return false;
+          } else if (monthOfBirth === 2 && dayOfBirth < 1) {
+            return false;
+          } else {
+            return true;
+          }
+        } else if (dayOfBirth < 1 || dayOfBirth > 31){
+          return false;
+        } else {
+          return true;
+        }
       }
-    }
-
+    
+     
+      let monthValid = monthValidator();
+      let dayValid = dayValidator();
+  
     
